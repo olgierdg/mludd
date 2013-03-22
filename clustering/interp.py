38d35aa9b453765c6a8d93c5613b48d6e42c466f
rzeszut <rzeszutekm@gmail.com>
2013-03-22 18:35:38 +0100

    Improved interpolation skeleton.

--- clustering/interp.py
+++ clustering/interp.py
@@ -3,6 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.interpolate as sp
+import matplotlib.dates as md
 
 def interpolate_data(data):
     t = data[:, 0]
@@ -12,26 +13,32 @@ def interpolate_data(data):
     # zeby to sie dalo do funkcji wrzucic, robie z dat
     # integery, pominalem dzien, miesiac i rok, bo byly takie
     # same dla calego zestawu danych
-    for ti in t:
-        tt = ti.time()
-        t_t.append(tt.hour * 3600 + tt.minute * 60 + tt.second)   
+#    for ti in t:
+#        tt = ti.time()
+#        t_t.append(tt.hour * 3600 + tt.minute * 60 + tt.second)   
+
+    t_t = np.array(map(md.date2num, t))
 
     # to bylo potrzebne wczesniej, na wszelki wypadek zostawiam
     #indices = np.argsort(x)
     #x = x[indices]
     #y = y[indices]
 
-    tck = sp.splrep(t_t,y,s=1)
+    # s = 0 -> wtedy nie pomija punktow
+    tck = sp.splrep(t_t, y, s = 0)
 
     # inne metody interpolacji
     #fl = sp.interp1d(t, y,kind='linear')
     #fc = sp.interp1d(t, y,kind='cubic')
     
-    tnew = data[:, 0]
-    ynew = sp.splev(t_t,tck,der=0)
-    
+    # tutaj sie tworzy nowy, "gestszy" czas. W zasadzie powinno byc co 1 min,
+    # ale zrobilem duzego linspejsa co by pokazac, ze splajny dzialaja.
+    t_tnew = np.linspace(np.min(t_t), np.max(t_t), 1000)
+    ynew = sp.splev(t_tnew,tck,der=0)
+    tnew = np.array(map(md.num2date, t_tnew))
+
     plt.figure()
-    plt.plot(t,y,'o',tnew,ynew)
+    plt.plot(t, y, 'ro', tnew, ynew)
     #plt.legend(['data', 'spline interpolation'], loc='lower right')
     plt.xlabel("datetime")
     plt.ylabel("latitude")
